6fabab65554b0e6fe2f8b4b6d690cfb67817d0e8
ShivGupta310 <shivgupta07.sg@gmail.com>
2025-12-28 18:50:17 +0530

    bencode parsing work ints done

---
+++ src/core/bencode.cpp
@@ -0,0 +1,88 @@
+#include "bencode.hpp"
+#include <stdexcept>
+#include <charconv>
+
+namespace Torrent::Core{
+
+    BValue parse_next(std::string_view& cursor){
+
+        if(cursor.empty()){
+            throw std::runtime_error("Empty cursor/buffer");
+        }
+
+        char lead = cursor[0];
+
+        switch(lead){
+            //int
+            case 'i':
+                cursor.remove_prefix(1);
+                //find end 'e'
+                size_t end = cursor.find('e');
+
+                if (end == std::string_view::npos){
+                    throw std::runtime_error("Invalid bencode int: missing 'e'");
+                }
+
+                //number substr
+                std::string_view num_str = cursor.substr(0, end);
+
+                long long val = std::stoll(std::string(num_str));
+
+                //adv cursor
+                cursor.remove_prefix(end + 1);
+
+                return BValue{ val };
+                break;
+            
+            //list
+            case 'l':
+                break;
+
+            //dict
+            case 'd':
+                break;
+            
+            default:
+                if (std::isdigit(lead)){
+
+                    size_t colon = cursor.find(':');
+                    if (colon == std::string_view::npos){
+                        throw std::runtime_error("Invalid bencode string: missing ':'");
+                    }
+
+                    std::string_view len_str = cursor.substr(0, colon);
+                    size_t len = 0;
+
+                    auto [ptr, ec] = std::from_chars(len_str.data(), len_str.data() + len_str.size(), len);
+                    
+                    if (ec == std::errc()) {
+                    
+                    } else if (ec == std::errc::invalid_argument) {
+                        throw std::runtime_error("Invalid string length: not a number");
+                    } else if (ec == std::errc::result_out_of_range) {
+                        throw std::runtime_error("String length out of range");
+                    }
+                
+                }
+                else{
+                    throw std::runtime_error("Invalid bencode lead character");
+                }
+        }
+
+    
+        //byte strings
+        else if (std::isdigit(cursor[0])){
+            size_t colon = cursor.find(':');
+            if (colon == std::string_view::npos){
+                throw std::runtime_error("Invalid bencode string: missing ':'");
+            }
+
+            std::string_view len_str = cursor.substr(0, colon);
+            
+
+        }
+
+
+    }
+
+}
---
+++ src/core/bencode.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <variant>
+#include <vector>
+#include <string>
+#include <map>
+#include <string_view>
+
+namespace Torrent::Core{
+
+	//BVal is a type compiler only gonna work w ptrs (vector, map) so it doesnt need to know size, forward declaration 
+	struct BValue;
+
+	//Bencode allows byte strings, ints, lists, dictionaries
+	using BList = std::vector<BValue>;
+	using BDict = std::map<std::string, BValue>;
+
+	struct BValue{
+   	 
+	  std::variant<long long, std::string, BList, BDict> data;	
+    };
+
+	//Function sig
+	BValue parse_next(std::string_view& cursor);
+
+}
